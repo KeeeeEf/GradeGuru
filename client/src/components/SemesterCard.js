ca29cd3d380a68757a6ac69b5b59c9c81cc5297f
Sealiosaurus <sealsplat@gmail.com>
2023-12-16 22:58:04 +0800

    [Seal][Feat] Added Semester Cards and backend integration for Semesters

--- client/src/components/SemesterCard.js
+++ client/src/components/SemesterCard.js
@@ -1,11 +1,13 @@
+// SemesterCard.js
+
 import React from 'react';
 
 const SemesterCard = ({ semester }) => {
   return (
-    <div className="border p-4 my-4">
-      <h3>{semester.name}</h3>
-      <p>Start Date: {semester.startDate}</p>
-      <p>End Date: {semester.endDate}</p>
+    <div className="border p-4 rounded-md shadow-md hover:shadow-lg transition duration-300">
+      <h3 className="text-xl font-semibold mb-2">{semester.semester}</h3>
+      <p className="text-gray-600">{semester.year}</p>
+      {/* Add more details as needed */}
     </div>
   );
 };
