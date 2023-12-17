e00560cc10ca002f4c7c82f2d3e295a2c0ffe7ef
Sealiosaurus <sealsplat@gmail.com>
2023-12-18 00:57:57 +0800

    [Seal][Fix] Adjusted SemesterTab display order

--- client/src/components/SemesterTab.js
+++ client/src/components/SemesterTab.js
@@ -4,9 +4,9 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import CourseList from './CourseList';
+import AddCourseModal from './AddCourseModal';
 
 function CustomTabPanel(props) {
-  
   const { children, value, index, ...other } = props;
 
   return (
@@ -36,29 +36,55 @@ const a11yProps = (index) => {
 };
 
 export default function SemesterTab({ semesters, selectedSemester, onSelectSemester, coursesData }) {
+  // Define the custom order for semesters
+  const semesterOrder = ['first', 'second', 'summer'];
+
+  // Sort the semesters based on the custom order
+  const sortedSemesters = semesters.sort((a, b) =>
+    semesterOrder.indexOf(a.semester) - semesterOrder.indexOf(b.semester)
+  );
+
   const [value, setValue] = React.useState(0);
+  const [isAddCourseModalOpen, setAddCourseModalOpen] = React.useState(false);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  return (
+  const openAddCourseModal = () => {
+    setAddCourseModalOpen(true);
+  };
 
+  const closeAddCourseModal = () => {
+    setAddCourseModalOpen(false);
+  };
+
+  return (
     <Box sx={{ width: '100%' }}>
       <Tabs value={value} onChange={handleChange} aria-label="semester tabs">
-        {semesters.map((semester, index) => (
+        {sortedSemesters.map((semester, index) => (
           <Tab
             key={index}
-            label={semester.semester === 'summer' ? 'Summer' : `${semester.semester.charAt(0).toUpperCase()}${semester.semester.slice(1)} Sem`}
+            label={
+              semester.semester === 'summer'
+                ? 'Summer'
+                : `${semester.semester.charAt(0).toUpperCase()}${semester.semester.slice(1)} Sem`
+            }
             {...a11yProps(index)}
           />
         ))}
       </Tabs>
-      {semesters.map((semester, index) => (
+      {sortedSemesters.map((semester, index) => (
         <CustomTabPanel key={index} value={value} index={index}>
           <CourseList semesterId={semester.sem_id} />
+          <button onClick={openAddCourseModal}>Add Course</button>
         </CustomTabPanel>
       ))}
+      <AddCourseModal
+        isOpen={isAddCourseModalOpen}
+        onClose={closeAddCourseModal}
+        semesterId={sortedSemesters[value].sem_id} // Pass the selected semester ID
+      />
     </Box>
   );
 }
