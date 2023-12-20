ea3d88b6238e2cd7a74bd7569a65cd9c7b4db59d
Sealiosaurus <sealsplat@gmail.com>
2023-12-21 03:19:59 +0800

    [Seal][Feat] Added alphabetical sort and support for DeleteCourseModal to CourseList

--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -1,46 +1,66 @@
-// components/CourseList.js
 import React, { useEffect, useState } from 'react';
 import CourseCard from './CourseCard';
 import axios from 'axios';
 import config from '../common/config';
+import DeleteCourseModal from './DeleteCourseModal';
 import { Link } from 'react-router-dom';
 
 const CourseList = ({ semesterId, onCourseAdded }) => {
   const [courses, setCourses] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const fetchCourses = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get(`${config.API}/courses/getCourseBySemester?semesterId=${semesterId}`);
+      // Sort the courses alphabetically by their names
+      const sortedCourses = response.data.data.sort((a, b) => a.course_name.localeCompare(b.course_name));
+      setCourses(sortedCourses);
+    } catch (err) {
+      setError('Error fetching courses');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchCourses = async () => {
-      setIsLoading(true);
-      try {
-        const response = await axios.get(`${config.API}/courses/getCourseBySemester?semesterId=${semesterId}`);
-        setCourses(response.data.data);
-      } catch (err) {
-        setError('Error fetching courses');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchCourses();
   }, [onCourseAdded]);
 
-  if (isLoading) {
-    return <p>Loading courses...</p>;
-  }
+  const handleDeleteCourse = async () => {
+    try {
+      const response = await axios.delete(`${config.API}/courses/deleteCourse/${selectedCourse}`);
+      fetchCourses();
+      setSelectedCourse(null);
+      setIsDeleteModalOpen(false);
+    } catch (err) {
+      console.error('Error deleting course:', err);
+    }
+  };
 
-  if (error) {
-    return <p>Error fetching courses: {error}</p>;
-  }
+  const handleCourseCardDeleteClick = (courseId) => {
+    setSelectedCourse(courseId);
+    setIsDeleteModalOpen(true);
+  };
 
   return (
     <div className="grid grid-cols-1 gap-4 w-full">
       {courses.map((course) => (
-        <Link to={`/course/${course.course_id}`} key={course.course_id}>
-          <CourseCard key={course.course_id} course={course} />
-        </Link>
+        <div key={course.course_id}>
+          <CourseCard course={course} onDelete={(courseId) => handleCourseCardDeleteClick(courseId)} />
+        </div>
       ))}
+      {selectedCourse && (
+        <DeleteCourseModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setIsDeleteModalOpen(false)}
+          onDeleteConfirm={handleDeleteCourse}
+          courseName={courses.find((course) => course.course_id === selectedCourse)?.course_name}
+        />
+      )}
     </div>
   );
 };
