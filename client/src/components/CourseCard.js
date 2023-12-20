20d1b0831732ddb70df3010dc1992c1b59f48c9f
Sealiosaurus <sealsplat@gmail.com>
2023-12-21 03:18:22 +0800

    [Seal][Feat] Added link to Course page and delete functionality for CourseCard

--- client/src/components/CourseCard.js
+++ client/src/components/CourseCard.js
@@ -1,10 +1,15 @@
-// components/CourseCard.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CourseCard = ({ course }) => {
+const CourseCard = ({ course, onDelete }) => {
   return (
-    <div className="border p-4 rounded-md shadow-md hover:shadow-lg transition duration-300">
-      <h3 className="text-xl font-semibold mb-2">{course.course_name}</h3>
+    <div className="border p-4 rounded-md shadow-md hover:shadow-lg transition duration-300 flex justify-between items-center">
+      <Link to={`/course/${course.course_id}`} key={course.course_id}>
+        <h3 className="text-xl font-semibold mb-2">{course.course_name}</h3>
+      </Link>
+      <button onClick={() => onDelete(course.course_id)} className="bg-red-500 text-white py-2 px-4 rounded">
+        Delete
+      </button>
     </div>
   );
 };
