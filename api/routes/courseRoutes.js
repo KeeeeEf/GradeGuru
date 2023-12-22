61a8fdb6a48aa4d5bf836362e78163c9190db48c
Sealiosaurus <sealsplat@gmail.com>
2023-12-23 01:21:28 +0800

    [Seal][Feat] Added route for getCourseUnits for courseRoutes

--- api/routes/courseRoutes.js
+++ api/routes/courseRoutes.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { getCourseBySemester, addCourseBySemester, deleteCourse } = require('../controller/courseController');
+const { getCourseBySemester, addCourseBySemester, deleteCourse, getCourseUnits } = require('../controller/courseController');
 
 router.get('/getCourseBySemester', getCourseBySemester);
 router.post('/addCourseBySemester', addCourseBySemester);
 router.delete('/deleteCourse/:courseId', deleteCourse);
+router.get('/getCourseUnits/:courseId', getCourseUnits);
 
 module.exports = router;
