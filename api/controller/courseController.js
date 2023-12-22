72a823f1afc8c7cf811ad249389a700300f3a15f
Sealiosaurus <sealsplat@gmail.com>
2023-12-23 01:17:30 +0800

    [Seal][Feat] Added getCourseUnits function for courseController

--- api/controller/courseController.js
+++ api/controller/courseController.js
@@ -33,58 +33,58 @@ const getCourseBySemester = (req, res) => {
 };
 
 const addCourseBySemester = (req, res) => {
-    try {
-      const { sem_id, course_name } = req.body;
-  
-      // Check for duplicate course names in the same semester
-      const checkDuplicateSQL = 'SELECT * FROM courses WHERE sem_id = ? AND course_name = ?';
-      const duplicateValues = [sem_id, course_name];
-  
-      db.query(checkDuplicateSQL, duplicateValues, (duplicateErr, duplicateResults) => {
-        if (duplicateErr) {
-          console.error('Error checking for duplicate course:', duplicateErr);
+  try {
+    const { sem_id, course_name, units } = req.body;
+
+    // Check for duplicate course names in the same semester
+    const checkDuplicateSQL = 'SELECT * FROM courses WHERE sem_id = ? AND course_name = ?';
+    const duplicateValues = [sem_id, course_name];
+
+    db.query(checkDuplicateSQL, duplicateValues, (duplicateErr, duplicateResults) => {
+      if (duplicateErr) {
+        console.error('Error checking for duplicate course:', duplicateErr);
+        return res.status(500).json({
+          success: false,
+          message: 'Internal Server Error',
+        });
+      }
+
+      if (duplicateResults.length > 0) {
+        // Duplicate course found
+        return res.status(400).json({
+          success: false,
+          message: 'Course with the same name already exists in the selected semester.',
+        });
+      }
+
+      // If no duplicates, proceed with adding the course
+      const insertSQL = 'INSERT INTO courses (sem_id, course_name, units) VALUES (?, ?, ?)';
+      const insertValues = [sem_id, course_name, units];
+
+      db.query(insertSQL, insertValues, (insertErr, result) => {
+        if (insertErr) {
+          console.error('Error adding course:', insertErr);
           return res.status(500).json({
             success: false,
             message: 'Internal Server Error',
           });
         }
-  
-        if (duplicateResults.length > 0) {
-          // Duplicate course found
-          return res.status(400).json({
-            success: false,
-            message: 'Course with the same name already exists in the selected semester.',
-          });
-        }
-  
-        // If no duplicates, proceed with adding the course
-        const insertSQL = 'INSERT INTO courses (sem_id, course_name) VALUES (?, ?)';
-        const insertValues = [sem_id, course_name];
-  
-        db.query(insertSQL, insertValues, (insertErr, result) => {
-          if (insertErr) {
-            console.error('Error adding course:', insertErr);
-            return res.status(500).json({
-              success: false,
-              message: 'Internal Server Error',
-            });
-          }
-  
-          res.status(201).json({
-            success: true,
-            message: 'Course added successfully',
-            data: result,
-          });
+
+        res.status(201).json({
+          success: true,
+          message: 'Course added successfully',
+          data: result,
         });
       });
-    } catch (error) {
-      console.error('Error adding course:', error);
-      res.status(500).json({
-        success: false,
-        message: 'Internal Server Error',
-      });
-    }
-  };
+    });
+  } catch (error) {
+    console.error('Error adding course:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
 
   const deleteCourse = (req, res) => {
     try {
@@ -140,4 +140,35 @@ const addCourseBySemester = (req, res) => {
     }
   };
 
-module.exports = { getCourseBySemester, addCourseBySemester, deleteCourse };
+  const getCourseUnits = async (req, res) => {
+    try {
+      const { courseId } = req.params;
+  
+      const sql = 'SELECT units FROM courses WHERE course_id = ?';
+      const values = [courseId];
+  
+      db.query(sql, values, (err, results) => {
+        if (err) {
+          console.error('Error getting course units:', err);
+          res.status(500).json({
+            success: false,
+            message: 'Internal Server Error',
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: 'Course units retrieved successfully',
+            units: results[0].units,
+          });
+        }
+      });
+    } catch (error) {
+      console.error('Error getting course units:', error);
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+      });
+    }
+  };
+
+module.exports = { getCourseBySemester, addCourseBySemester, deleteCourse, getCourseUnits };
