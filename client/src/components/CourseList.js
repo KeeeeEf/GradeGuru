c356ae96b5430ba4f02be7f39a833d04e3c66b0b
Sealiosaurus <sealsplat@gmail.com>
2023-12-23 01:25:02 +0800

    [Seal][Feat] Added labels for CourseList and logic for GPA calculation

--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -3,7 +3,7 @@ import CourseCard from './CourseCard';
 import axios from 'axios';
 import config from '../common/config';
 import DeleteCourseModal from './DeleteCourseModal';
-import { Link } from 'react-router-dom';
+import { fetchGradesForCourse, fetchCourseUnits } from './utils';
 
 const CourseList = ({ semesterId, onCourseAdded }) => {
   const [courses, setCourses] = useState([]);
@@ -11,6 +11,7 @@ const CourseList = ({ semesterId, onCourseAdded }) => {
   const [error, setError] = useState('');
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [semesterGPA, setSemesterGPA] = useState(null);
 
   const fetchCourses = async () => {
     setIsLoading(true);
@@ -26,9 +27,49 @@ const CourseList = ({ semesterId, onCourseAdded }) => {
     }
   };
 
+  const fetchSemesterGPA = async () => {
+    let totalGPA = 0;
+    let totalUnits = 0;
+
+    for (const course of courses) {
+      const grades = await fetchGradesForCourse(course.course_id, 'finals'); // Assuming a function to fetch all grades
+
+      console.log('Grades for course', course.course_name, ':', grades);
+      const units = await fetchCourseUnits(course.course_id);
+
+      if (grades && units) {
+        const courseGPA = calculateCourseGPA(grades, units);
+        totalGPA += courseGPA * units;
+        totalUnits += units;
+      }
+    }
+
+    const semesterGPA = totalUnits !== 0 ? totalGPA / totalUnits : 0;
+    setSemesterGPA(parseFloat(semesterGPA.toFixed(2)));
+  };
+
+  const calculateCourseGPA = (grades, units) => {
+    // Ensure that grades is an array
+    const gradesArray = Array.isArray(grades) ? grades : [grades];
+  
+    let totalScore = 0;
+  
+    gradesArray.forEach((grade) => {
+      totalScore += grade.grade.scale * units;
+    });
+  
+    const totalUnits = gradesArray.reduce((total, grade) => total + units, 0);
+  
+    return totalUnits !== 0 ? totalScore / totalUnits : 0;
+  };
+
   useEffect(() => {
     fetchCourses();
   }, [onCourseAdded]);
+
+  useEffect(() => {
+    fetchSemesterGPA();
+  }, [courses]);
 
   const handleDeleteCourse = async () => {
     try {
@@ -48,8 +89,26 @@ const CourseList = ({ semesterId, onCourseAdded }) => {
 
   return (
     <div className="grid grid-cols-1 gap-4 w-full">
+      <div className="grid grid-cols-7">
+        <div className="col-span-3">
+          <h3 className="text-xl font-semibold">Course Name</h3>
+        </div>
+        <div className="col-span-1 flex justify-center">
+          <p>Units</p>
+        </div>
+        <div className="col-span-1 flex justify-center">
+          <p>Midterm Grade</p>
+        </div>
+        <div className="col-span-1 flex justify-center">
+          <p>Final Grade</p>
+        </div>
+        <div className="col-span-1 flex justify-center">
+          <p></p>
+        </div>
+      </div>
       {courses.map((course) => (
         <div key={course.course_id}>
+          
           <CourseCard course={course} onDelete={(courseId) => handleCourseCardDeleteClick(courseId)} />
         </div>
       ))}
@@ -61,6 +120,17 @@ const CourseList = ({ semesterId, onCourseAdded }) => {
           courseName={courses.find((course) => course.course_id === selectedCourse)?.course_name}
         />
       )}
+      <div className="grid grid-cols-7">
+      <div className="flex col-span-4 justify-center">
+          <p></p>
+        </div>
+        <div className="flex col-span-1 justify-end">
+        <h3 className="text-xl font-semibold">GPA:</h3>
+        </div>
+        <div className="flex col-span-1 justify-center">
+        <h3 className="text-xl font-semibold">{semesterGPA}</h3>
+        </div>
+        </div>
     </div>
   );
 };
