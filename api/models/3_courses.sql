1edccc2df267fb32017c387d64ae92ac903b7e92
Sealiosaurus <sealsplat@gmail.com>
2023-12-23 01:18:31 +0800

    [Seal][Feat] Added units to courses table

--- api/models/3_courses.sql
+++ api/models/3_courses.sql
@@ -2,6 +2,7 @@ CREATE TABLE `courses` (
   `course_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `sem_id` bigint(20) NOT NULL,
   `course_name` varchar(255) NOT NULL,
+  `units` int NOT NULL,
   PRIMARY KEY (`course_id`),
   KEY `sem_id` (`sem_id`),
   CONSTRAINT `sem_idfk_1` FOREIGN KEY (`sem_id`) REFERENCES `semester` (`sem_id`) ON DELETE CASCADE
