73a9dd28d959b9a9f39cf8428bf32ace6350d78c
Sealiosaurus <sealsplat@gmail.com>
2023-12-23 01:19:23 +0800

    [Seal][Feat] Added ON DELETE CASCADE to activity table

--- api/models/6_activity.sql
+++ api/models/6_activity.sql
@@ -9,6 +9,6 @@ CREATE TABLE `activity` (
   PRIMARY KEY (`activity_id`),
   KEY `course_id` (`course_id`),
   KEY `criteria_id` (`criteria_id`),
-  CONSTRAINT `course_idfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`),
-  CONSTRAINT `criteria_idfk_1` FOREIGN KEY (`criteria_id`) REFERENCES `criteria` (`criteria_id`)
+  CONSTRAINT `course_idfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE,
+  CONSTRAINT `criteria_idfk_1` FOREIGN KEY (`criteria_id`) REFERENCES `criteria` (`criteria_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
